f259592ea37700e415d29dd217f1c494d83c700f
hyano <1297631+hyano@users.noreply.github.com>
2022-04-17 12:19:50 +0900

    ADPCM: fixed end/limit address checking

--- src/ymfm_adpcm.h
+++ src/ymfm_adpcm.h
@@ -342,10 +342,10 @@ private:
 	void load_start();
 
 	// limit checker
-	bool at_limit() const { return (m_curaddress >> address_shift()) >= m_regs.limit(); }
+	bool at_limit() const { return m_curaddress == (((m_regs.limit() + 1) << address_shift())); }
 
 	// end checker
-	bool at_end() const { return (m_curaddress >> address_shift()) > m_regs.end(); }
+	bool at_end() const { return m_curaddress == (((m_regs.end() + 1) << address_shift())); }
 
 	// internal state
 	uint32_t const m_address_shift; // address bits shift-left
